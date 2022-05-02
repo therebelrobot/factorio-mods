f7af78db3c24d646fb54a0f0646bb5cd911b7b18
Aster Haven (@therebelrobot) <asterhaven@pm.me>
2022-05-01 18:01:08 -0700

    another try

--- mods/dark-matter-replicators-18_1.1.2/prototypes/replications/bob.lua
+++ mods/dark-matter-replicators-18_1.1.2/prototypes/replications/bob.lua
@@ -356,6 +356,34 @@ if bobmods.logistics then
 	repltech_recipe("basic-transport-belt", "device3")
 	repltech_recipe("turbo-transport-belt", "device3", {upgrade = true})
 	repltech_recipe("ultimate-transport-belt", "device3", {upgrade = true})
+
+	repltech_recipe("ru-behemoth-transport-belt", "device3")
+	repltech_recipe("ru-behemoth-underground-belt", "device3")
+	repltech_recipe("ru-behemoth-loader", "device3")
+	repltech_recipe("ru-behemoth-inserter", "device3")
+	repltech_recipe("ru-behemoth-lamp", "device3")
+	repltech_recipe("ru-behemoth-solar-panel", "device3")
+	repltech_recipe("ru-behemoth-substation", "device3")
+	repltech_recipe("ru-behemoth-pipe", "device3")
+	repltech_recipe("ru-behemoth-pipe-to-ground", "device3")
+	repltech_recipe("ru-behemoth-substation", "device3")
+	repltech_recipe("ru-behemoth-accumulator", "device3")
+	repltech_recipe("ru-behemoth-splitter", "device3")
+	repltech_recipe("bob-construction-robot-5", "device3")
+	repltech_recipe("bob-logistic-robot-5", "device3")
+	repltech_recipe("speed-module-8", "device3")
+	repltech_recipe("micro-assembler-6", "device3")
+	repltech_raw("alien-goo", 0.02)
+
+	repltech_recipe("micro-assembler-6", "device3")
+	repltech_recipe("micro-assembler-6", "device3")
+
+		
+	--Fish
+	repltech_recipe("alien-fish-1-raw", "shape")
+	repltech_recipe("crystal-splinter-blue-cut", "shape")
+	repltech_recipe("crystal-splinter-green-cut", "shape")
+	repltech_recipe("crystal-splinter-red-cut", "shape")
 	
 	if bobmods.warfare and bobmods.warfare.RobotParts then
 		--Robot brains
